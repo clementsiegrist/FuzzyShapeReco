14896b7be8fc6cb4cd766b969b0e60d561952841
clementsiegrist <36403339+clementsiegrist@users.noreply.github.com>
2020-12-18 13:11:57 +0100

    Update __init__.py

--- anfis/membership/__init__.py
+++ anfis/membership/__init__.py
@@ -1,2 +1,2 @@
-from anfis.membership import membershipfunction
-from anfis.membership import mfDerivs
+from reco_formes.anfis.membership import membershipfunction
+from reco_formes.anfis.membership import mfDerivs
